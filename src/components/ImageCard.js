53cd56e363544385f38bb3657616b6c7200efa17
Anthony Gregis <anthony@thedevtron.com>
2020-10-09 20:25:08 -0500

    [Portfolio] Add header info, site cards

---
+++ src/components/ImageCard.js
@@ -0,0 +1,123 @@
+import React from "react"
+
+// Material UI Imports
+import {makeStyles} from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Typography from "@material-ui/core/Typography"
+import PropTypes from 'prop-types';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { useSpring, animated } from 'react-spring/web.cjs';
+
+const useStyles = makeStyles({
+	root: {
+		maxWidth: 345,
+		border: "1px solid white"
+	},
+	media: {
+		height: 140,
+	},
+	modal: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		maxWidth: "none",
+	},
+})
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+	const { in: open, children, onEnter, onExited, ...other } = props;
+	const style = useSpring({
+		from: { opacity: 0 },
+		to: { opacity: open ? 1 : 0 },
+		onStart: () => {
+			if (open && onEnter) {
+				onEnter();
+			}
+		},
+		onRest: () => {
+			if (!open && onExited) {
+				onExited();
+			}
+		},
+	});
+
+	return (
+		<animated.div ref={ref} style={style} {...other}>
+			{children}
+		</animated.div>
+	);
+});
+
+Fade.propTypes = {
+	children: PropTypes.element,
+	in: PropTypes.bool.isRequired,
+	onEnter: PropTypes.func,
+	onExited: PropTypes.func,
+};
+
+export function SpringModal(props) {
+	const classes = useStyles();
+
+	return (
+		<div>
+			<Modal
+				aria-labelledby="spring-modal-title"
+				aria-describedby="spring-modal-description"
+				className={classes.modal}
+				open={props.open}
+				onClose={props.handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}
+			>
+				<Fade in={props.open}>
+					<iframe title={props.site} src={"http://anthonygregis.com/sites/" + props.site} width="1000vh" height="500vh" />
+				</Fade>
+			</Modal>
+		</div>
+	);
+}
+
+const ImageCard = props => {
+	const classes = useStyles()
+	const [open, setOpen] = React.useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<>
+			<SpringModal open={open} handleClose={handleClose} site={props.site} />
+			<Card className={classes.root} onClick={handleOpen}>
+				<CardActionArea>
+					<CardMedia
+						className={classes.media}
+						image={props.image}
+						title={props.name}
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{props.name}
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="p">
+							{props.description}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</>
+	)
+}
+
+export default ImageCard
---
+++ src/pages/Home.js
@@ -0,0 +1,85 @@
+import React from "react"
+
+// Material UI Imports
+import Grid from "@material-ui/core/Grid"
+import {makeStyles} from "@material-ui/core/styles"
+
+// Component Imports
+import ImageCard from "../components/ImageCard"
+
+const useStyles = makeStyles((theme) => ({
+	grid: {
+		marginTop: "2vh"
+	},
+	roundImage: {
+		borderRadius: "50%",
+	},
+}))
+
+const Home = () => {
+	const classes = useStyles()
+
+	return (
+		<Grid container spacing={1} className={classes.grid}>
+			<Grid item xs={12} align="center">
+				<img src={"https://pbs.twimg.com/profile_images/1112076519965425665/4GjtH4Ry_400x400.jpg"}
+				     alt="Anthony Gregis infront of a red 'Be Next' Maingear backdrop" width="90px"
+				     className={classes.roundImage}/>
+				<h1>Anthony Gregis</h1>
+				<h5>A Software Engineer propelled by solving complex situations.</h5>
+			</Grid>
+			<Grid item xs={12} align="center">
+				<h1>Projects</h1>
+			</Grid>
+			<Grid item xs={12} align="center">
+				<h3>Pre Codecamp</h3>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard
+					name="[NoPixel] Chop List"
+					description="A way for players ingame to look up certain vehicles. Designed to look rugged like a thrown together dark web site."
+					image="/images/ChopList.png"
+					site="/ChopList/index.php"
+				/>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard
+					name="[NoPixel] DabCoin"
+					description="A recreation of a cryptocurrency stock market, designed with random fluctuating prices, user wallets, public transaction records, and trading."
+					image="/images/Dabcoin.png"
+					site="/dabcoin/login.php"
+				/>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard
+					name="[NoPixel] Payphone"
+					description="Map of GTA5 with all the payphone locations, used by dark web characters in the server to find ways to make descreet phone calls."
+					image="/images/payphone.png"
+					site="/payphone/index.php"
+				/>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard
+					name="[NoPixel] Kifflom"
+					description="Recreates the kifflom website from GTAV. The contact us page displays a random phone number each time and can be used in the give money page to enter the dark web."
+					image="/images/Kifflom.png"
+					site="/kifflom/KIFFLOM.html"
+				/>
+			</Grid>
+			<Grid item xs={12} align="center">
+				<h3>During / Post Codecamp</h3>
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard name="Project" description="A unique javascript project" image="https://pbs.twimg.com/profile_images/1112076519965425665/4GjtH4Ry_400x400.jpg" />
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard name="Project" description="A unique javascript project" image="https://pbs.twimg.com/profile_images/1112076519965425665/4GjtH4Ry_400x400.jpg" />
+			</Grid>
+			<Grid item xs={12} sm={6} md={4} lg={3} align="center">
+				<ImageCard name="Project" description="A unique javascript project" image="https://pbs.twimg.com/profile_images/1112076519965425665/4GjtH4Ry_400x400.jpg" />
+			</Grid>
+		</Grid>
+	)
+}
+
+export default Home
